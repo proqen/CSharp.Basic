dd4eb00c30c561c0682081fd9ebee0c8f174b4ae
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-26 13:34:24 +0600

    craete task6.4

---
+++ Task6.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task6._4
+{
+    static class Extension
+    {
+        public static void Sorting(this int [] array)
+        {
+            Array.Sort(array);
+            foreach (var item in array)
+                Console.WriteLine(item);
+        }
+    }
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var arrayInt = new int[] { 2, 3, 1, 4, 5, 2, 67, 12, 3 };
+            arrayInt.Sorting();
+        }
+    }
+}
