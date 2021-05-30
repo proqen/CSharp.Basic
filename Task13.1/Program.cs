1a374734f00ca0a18b6384467a9ca488f460b701
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-30 23:00:44 +0600

    fix

--- Task13.1/Program.cs
+++ Task13.1/Program.cs
@@ -8,19 +8,22 @@ namespace Task13._1
 {
     class Program
     {
+        static readonly object locker = new object();  //Объект синхронизации доступа к разделяемому ресурсу (объект блокировки)
+
         static void Main(string[] args)
         {
+            Console.SetWindowSize(100, 30);
             Console.OutputEncoding = Encoding.UTF8;
             Console.CursorVisible = false;
 
-            foreach (var item in Enumerable.Range(1, 40)) 
+            foreach (var item in Enumerable.Range(1, 100)) 
             {
                 new Thread(NewMatrix).Start();
             }
         }
         static void NewMatrix()
         {
-            char[] yMatrix;
+            string yMatrix = " ";
             int xPosition, yPosition, sleep, length;
 
             while (true)
@@ -29,36 +32,34 @@ namespace Task13._1
                 yPosition = 0;
                 sleep = new Random().Next(50, 500);
                 length = new Random().Next(3, 10);
-                yMatrix = new char[length];
 
                 while (true)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-
                     for (var i = 0; i < length; i++)
-                        yMatrix[i] = (char)new Random().Next(33, 126);
+                        yMatrix += ((char)new Random().Next(33, 126)).ToString();
 
-                    for (var i = 0; i < length; i++)
+                    lock (locker)
                     {
-                        if (i == length - 2) Console.ForegroundColor = ConsoleColor.Green;
-                        if (i == length - 1) Console.ForegroundColor = ConsoleColor.White;
-                        Console.SetCursorPosition(xPosition, yPosition + i);
-                        Console.Write(yMatrix[i]);
-                    }
-
-                    Console.SetCursorPosition(xPosition, yPosition);
-                    Console.Write(' ');
-
-                    if (yPosition++ >= Console.WindowHeight - length - 1)
-                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
                         for (var i = 0; i < length; i++)
                         {
+                            if (i == length - 2) Console.ForegroundColor = ConsoleColor.Green;
+                            if (i == length - 1) Console.ForegroundColor = ConsoleColor.White;
                             Console.SetCursorPosition(xPosition, yPosition + i);
-                            Console.Write(' ');
+                            Console.Write(yMatrix[i]);
                         }
-                        break;
+
+                        if (yPosition++ >= Console.WindowHeight - length - 1)
+                        {
+                            for (var i = 0; i < length; i++)
+                            {
+                                Console.SetCursorPosition(xPosition, yPosition + i);
+                                Console.Write(' ');
+                            }
+                            break;
+                        }
                     }
-                    yMatrix.DefaultIfEmpty();
+                    yMatrix = " ";
                     Thread.Sleep(sleep);
                 }
             }
