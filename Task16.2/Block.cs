f2c2acaae2563dd73cc01fdd150dc95a63e0d65e
Камчыбек Абдыназаров <tilekberdiev@outlook.com>
2021-05-31 18:29:58 +0600

    create Task16.2

---
+++ Task16.2/Block.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16._2
+{
+    class Block
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || this.GetType() != obj.GetType())
+                return false;
+
+            Block p = (Block)obj;
+            return (Id == p.Id) && (Name == p.Name) && (Email == p.Email) && (City == p.City);
+        }
+        public override string ToString()
+        {
+            return $"Id = {Id},Name = {Name}, Email = {Email}, City = {City}";
+        }
+    }
+}
---
+++ Task16.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task16._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Block block = new Block { Id = 1, Name = "Kamchybek", Email = "email@email.com", City = "Bishkek" };
+            Console.WriteLine(block.Equals(block));
+            Console.WriteLine(block.ToString());
+        }
+    }
+}
